5f499ba2bf2a289d614dbaef48b423708ad0305a
Vytautas Valiukonis <vytautas.valiukonis@outlook.com>
2017-03-05 23:33:46 +0200

    Fixed setup.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name='steelbar',
     packages=['steelbar'],
-    version='1.0',
+    version='1.1',
     description='Simple multilayered progress bar for Python 3.',
     author='Vytautas Valiukonis',
     author_email='vytautas.valiukonis@outlook.com',
     url='https://github.com/SSteel2/SteelBar',
-    download_url='https://github.com/SSteel2/SteelBar/archive/1.0.tar.gz',
+    download_url='https://github.com/SSteel2/SteelBar/archive/1.1.tar.gz',
     keywords=['progress', 'bar', 'steelbar', 'multilayer'],
     classifiers=[],
 )
